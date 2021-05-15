4fe87c255be3609a75e1bfa4e44567e3847fc49d
p697 <254139147@qq.com>
2021-05-15 16:33:40 +0800

    chore: update @typescript-eslint

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,3 +1,4 @@
+
 export default {
   pages: [
     'pages/index/index'
@@ -7,5 +8,31 @@ export default {
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
-  }
+  },
+  tabBar: {
+    color: "#606468",
+    selectedColor: "#0089ff",
+    backgroundColor: '#fff',
+    // borderStyle: 'white',
+    custom: false,
+    list: [{
+      pagePath: 'pages/event/index',
+      text: 'Wiki',
+      iconPath: 'assets/tab/event.png',
+      selectedIconPath: 'assets/tab/event_active.png',
+    },
+    {
+      pagePath: 'pages/schedule/index',
+      text: 'Game',
+      iconPath: 'assets/tab/schedule.png',
+      selectedIconPath: 'assets/tab/schedule_active.png',
+    },
+    {
+      pagePath: 'pages/home/index',
+      text: 'About',
+      iconPath: 'assets/tab/home.png',
+      selectedIconPath: 'assets/tab/home_active.png',
+    },
+    ]
+  },
 }
